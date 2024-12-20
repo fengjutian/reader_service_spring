7f8b8e48cc5b01029d653b5c41639a330555fb63
fengjutian <2640111081@qq.com>
2024-12-20 11:29:12 +0800

    first commit

---
+++ src/main/java/com/fengjutian/reader_service_java/ReaderServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fengjutian.reader_service_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderServiceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderServiceJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fengjutian/reader_service_java/ReaderServiceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengjutian.reader_service_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderServiceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
